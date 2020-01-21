eb7977d644c285bf0384f2a8759996d33b6b3c2b
Renato Pacheco <renato.pacheco@adm.unip.br>
2020-01-21 15:31:20 -0300

    Adding NumberOfItemsEqualIsValid

---
+++ src/Extends/NumberOfItemsEqualIsValidExt.cs
@@ -0,0 +1,86 @@
+﻿using BitHelp.Core.Validation.Resources;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace BitHelp.Core.Validation.Extends
+{
+    public static class NumberOfItemsEqualIsValidExt
+    {
+        public static ValidationNotification NumberOfItemsEqualIsValid<TClasse>(
+            this ValidationNotification source, TClasse data, string reference, Expression<Func<TClasse, IList>> expression,
+                Expression<Func<TClasse, IList>> expressionCompare, params Expression<Func<TClasse, IList>>[] expressionList)
+            where TClasse : class
+        {
+            IList<IList> values = new List<IList>();
+
+            values.Add(expression.Compile().DynamicInvoke(data) as IList);
+            values.Add(expressionCompare.Compile().DynamicInvoke(data) as IList);
+
+            foreach (var item in expressionList)
+            {
+                values.Add(item.Compile().DynamicInvoke(data) as IList);
+            }
+            return source.NumberOfItemsEqualIsValid(reference, values);
+        }
+
+        public static ValidationNotification NumberOfItemsEqualIsValid(
+            this ValidationNotification source, string reference, IList value, IList valueCompare, params IList[] valueList)
+        {
+            valueList = valueList.Concat(new IList[] { value, valueCompare }).ToArray();
+            return source.NumberOfItemsEqualIsValid(reference, valueList);
+        }
+
+        private static ValidationNotification NumberOfItemsEqualIsValid(
+            this ValidationNotification source, string reference, IEnumerable<IList> value)
+        {
+            bool result = true;
+            int? total = null;
+            bool isnull = false;
+            
+            foreach (var item in value)
+            {
+                if(item != null)
+                {
+                    if(isnull)
+                    {
+                        result = false;
+                        break;
+                    }
+
+                    int current = (item as IList).Count;
+                    if (total == null)
+                    {
+                        total = current;
+                    }
+                    else if (current != total)
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+                else
+                {
+                    isnull = true;
+                    if (total != null)
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+
+            source.LastMessage = null;
+            if (!result)
+            {
+                string text = Resource.NumberOfItemsEqualInvalid;
+                var message = new ValidationMessage(text, reference);
+                source.LastMessage = message;
+                source.Add(message);
+            }
+            return source;
+        }
+    }
+}
---
+++ test/ExtendsTest/NumberOfItemsEqualIsValidExtTest.cs
@@ -0,0 +1,91 @@
+﻿using BitHelp.Core.Validation.Extends;
+using BitHelp.Core.Validation.Test.Resources;
+using Xunit;
+
+namespace BitHelp.Core.Validation.Test.ExtendsTest
+{
+    public class NumberOfItemsEqualIsValidExtTest
+    {
+        readonly ValidationNotification notification = new ValidationNotification();
+
+        [Fact]
+        public void Check_number_itens_is_valid()
+        {
+            ArrayValues array = new ArrayValues();
+            array.Bool = new bool[] { true, false, true };
+            array.Char = new char[] { 'a', 'b', 'c' };
+
+            notification.Clear();
+            this.notification.NumberOfItemsEqualIsValid(null, array.Bool, array.Char);
+            Assert.True(notification.IsValid());
+
+            notification.Clear();
+            this.notification.NumberOfItemsEqualIsValid(array, null, x => x.Bool, y => y.Char);
+            Assert.True(notification.IsValid());
+        }
+
+        [Fact]
+        public void Check_number_itens_is_invalid()
+        {
+            ArrayValues array = new ArrayValues();
+            array.Bool = new bool[] { true, false, true };
+            array.Char = new char[] { 'a', 'b' };
+
+            notification.Clear();
+            this.notification.NumberOfItemsEqualIsValid(null, array.Bool, array.Char);
+            Assert.False(notification.IsValid());
+
+            notification.Clear();
+            this.notification.NumberOfItemsEqualIsValid(array, null, x => x.Bool, y => y.Char);
+            Assert.False(notification.IsValid());
+        }
+
+        [Fact]
+        public void Check_all_values_null_is_valid()
+        {
+            ArrayValues array = new ArrayValues();
+            array.Bool = null;
+            array.Char = null;
+
+            notification.Clear();
+            this.notification.NumberOfItemsEqualIsValid(null, array.Bool, array.Char);
+            Assert.True(notification.IsValid());
+
+            notification.Clear();
+            this.notification.NumberOfItemsEqualIsValid(array, null, x => x.Bool, y => y.Char);
+            Assert.True(notification.IsValid());
+        }
+
+        [Fact]
+        public void Check_onbne_value_null_is_invalid()
+        {
+            ArrayValues array = new ArrayValues();
+            array.Bool = new bool[] { true, false, true };
+            array.Char = null;
+
+            notification.Clear();
+            this.notification.NumberOfItemsEqualIsValid(null, array.Bool, array.Char);
+            Assert.False(notification.IsValid());
+
+            notification.Clear();
+            this.notification.NumberOfItemsEqualIsValid(array, null, x => x.Bool, y => y.Char);
+            Assert.False(notification.IsValid());
+        }
+
+        [Fact]
+        public void Check_onbne_value_null_and_other_empty_is_invalid()
+        {
+            ArrayValues array = new ArrayValues();
+            array.Bool = new bool[] { };
+            array.Char = null;
+
+            notification.Clear();
+            this.notification.NumberOfItemsEqualIsValid(null, array.Bool, array.Char);
+            Assert.False(notification.IsValid());
+
+            notification.Clear();
+            this.notification.NumberOfItemsEqualIsValid(array, null, x => x.Bool, y => y.Char);
+            Assert.False(notification.IsValid());
+        }
+    }
+}
