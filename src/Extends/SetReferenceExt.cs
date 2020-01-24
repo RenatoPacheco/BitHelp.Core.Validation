00af68be0f2ce1981c940419634a8c4c1532269a
Renato Pacheco <renato.pacheco@adm.unip.br>
2020-01-24 09:57:29 -0300

    Adding SetReference

---
+++ src/Extends/SetReferenceExt.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using BitHelp.Core.Extend;
+using System.Linq.Expressions;
+
+namespace BitHelp.Core.Validation.Extends
+{
+    public static class SetReferenceExt
+    {
+        public static ValidationNotification SetReference<TClass>(
+            this ValidationNotification source, Expression<Func<TClass, object>> expression)
+        {
+            if(source.LastMessage != null)
+                source.LastMessage.Reference = expression.PropertyTrail();
+
+            return source;
+        }
+
+        public static ValidationNotification SetReference<TClass>(
+            this ValidationNotification source, TClass _, Expression<Func<TClass, object>> expression)
+        {
+            if (source.LastMessage != null)
+                source.LastMessage.Reference = expression.PropertyTrail();
+
+            return source;
+        }
+
+        public static ValidationNotification SetReference(
+            this ValidationNotification source, string reference)
+        {
+            if (source.LastMessage != null)
+                source.LastMessage.Reference = reference?.Trim();
+
+            return source;
+        }
+    }
+}
---
+++ test/ExtendsTest/SetReferenceExtTest.cs
@@ -0,0 +1,123 @@
+﻿using BitHelp.Core.Validation.Extends;
+using BitHelp.Core.Validation.Test.Resources;
+using Xunit;
+
+namespace BitHelp.Core.Validation.Test.ExtendsTest
+{
+    public class SetReferenceExtTest
+    {
+        readonly ValidationNotification notification = new ValidationNotification();
+
+        [Fact]
+        public void Check_set_reference_valid()
+        {
+            var single = new SingleValues
+            {
+                String = "number"
+            };
+            string reference = "New reference";
+
+            notification.Clear();
+            notification.LongIsValid(single.String).SetReference(reference);
+
+            Assert.False(notification.IsValid());
+            Assert.Equal(reference, notification.Messages[0].Reference);
+
+            notification.Clear();
+            notification.LongIsValid(single, x => x.String).SetReference(reference);
+
+            Assert.False(notification.IsValid());
+            Assert.Equal(reference, notification.Messages[0].Reference);
+        }
+
+        [Fact]
+        public void Check_set_reference_null_valid()
+        {
+            var single = new SingleValues
+            {
+                String = "number"
+            };
+            string reference = null;
+
+            notification.Clear();
+            notification.LongIsValid(single.String).SetReference(reference);
+
+            Assert.False(notification.IsValid());
+            Assert.Equal(reference, notification.Messages[0].Reference);
+
+            notification.Clear();
+            notification.LongIsValid(single, x => x.String).SetReference(reference);
+
+            Assert.False(notification.IsValid());
+            Assert.Equal(reference, notification.Messages[0].Reference);
+        }
+
+        [Fact]
+        public void Check_set_reference_empty_valid()
+        {
+            var single = new SingleValues
+            {
+                String = "number"
+            };
+            string reference = string.Empty;
+
+            notification.Clear();
+            notification.LongIsValid(single.String).SetReference(reference);
+
+            Assert.False(notification.IsValid());
+            Assert.Equal(reference, notification.Messages[0].Reference);
+
+            notification.Clear();
+            notification.LongIsValid(single, x => x.String).SetReference(reference);
+
+            Assert.False(notification.IsValid());
+            Assert.Equal(reference, notification.Messages[0].Reference);
+        }
+
+
+        [Fact]
+        public void Check_set_reference_empty_with_space_valid()
+        {
+            var single = new SingleValues
+            {
+                String = "number"
+            };
+            string reference = "         ";
+
+            notification.Clear();
+            notification.LongIsValid(single.String).SetReference(reference);
+
+            Assert.False(notification.IsValid());
+            Assert.Equal(string.Empty, notification.Messages[0].Reference);
+
+            notification.Clear();
+            notification.LongIsValid(single, x => x.String).SetReference(reference);
+
+            Assert.False(notification.IsValid());
+            Assert.Equal(string.Empty, notification.Messages[0].Reference);
+        }
+
+        [Fact]
+        public void Check_set_reference_expression_valid()
+        {
+            var single = new SingleValues
+            {
+                String = "number"
+            };
+
+            notification.Clear();
+            notification.LongIsValid(single, x => x.String)
+                    .SetReference<SingleValues>(x => x.Bool);
+
+            Assert.False(notification.IsValid());
+            Assert.Equal(nameof(single.Bool), notification.Messages[0].Reference);
+
+            notification.Clear();
+            notification.LongIsValid(single, x => x.String)
+                .SetReference(single, x => x.Bool);
+
+            Assert.False(notification.IsValid());
+            Assert.Equal(nameof(single.Bool), notification.Messages[0].Reference);
+        }
+    }
+}
