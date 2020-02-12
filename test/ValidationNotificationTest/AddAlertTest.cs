8baa3d62a83be321ed6662b0c272363064e81b3e
Renato Pacheco <renato.pacheco@adm.unip.br>
2020-02-12 14:46:26 -0300

    Adding tests to AddAlert, AddError, AddFatal, AddInfo, AddSuccess and AddUnauthorized

---
+++ test/ValidationNotificationTest/AddAlertTest.cs
@@ -0,0 +1,116 @@
+﻿using Xunit;
+using System;
+using System.Linq;
+using BitHelp.Core.Extend;
+using System.Linq.Expressions;
+using BitHelp.Core.Validation.Test.Resources;
+
+namespace BitHelp.Core.Validation.Test.ValidationNotificationTest
+{
+    public class AddAlertTest
+    {
+        readonly ValidationNotification _notification = new ValidationNotification();
+
+        private string GetReference<T>(Expression<Func<T, object>> expression)
+        {
+            return expression.PropertyTrail();
+        }
+        
+        private string GetDisplay<T>(Expression<Func<T, object>> expression)
+        {
+            return expression.PropertyDisplay();
+        }
+
+        [Fact]
+        public void Check_expression_default()
+        {
+            _notification.Clear();
+            _notification.AddAlert<SingleValues>(x => x.BoolNull);
+
+            var reference = GetReference<SingleValues>(x => x.BoolNull);
+            var result = _notification.Messages.First();
+
+            Assert.Equal(reference, result.Reference);
+            Assert.Null(result.Exception);
+            Assert.Equal(ValidationType.Alert, result.Type);
+        }
+
+        [Fact]
+        public void Check_expression_set_message()
+        {
+            _notification.Clear();
+            _notification.AddAlert<SingleValues>(x => x.BoolNull, message: "Message here");
+
+            var result = _notification.Messages.First();
+
+            Assert.Equal("Message here", result.Message);
+            Assert.Equal(ValidationType.Alert, result.Type);
+        }
+
+        [Fact]
+        public void Check_expression_set_message_with_display_name()
+        {
+            _notification.Clear();
+            _notification.AddAlert<SingleValues>(x => x.BoolNull, message: "{0} message here");
+
+            var display = GetDisplay<SingleValues>(x => x.BoolNull);
+            var message = string.Format("{0} message here", display);
+            var result = _notification.Messages.First();
+
+            Assert.Equal(message, result.Message);
+            Assert.Equal(ValidationType.Alert, result.Type);
+        }
+
+        [Fact]
+        public void Check_expression_set_reference()
+        {
+            _notification.Clear();
+            _notification.AddAlert<SingleValues>(x => x.BoolNull, reference: "reference");
+
+            var result = _notification.Messages.First();
+
+            Assert.Equal("reference", result.Reference);
+            Assert.Equal(ValidationType.Alert, result.Type);
+        }
+
+        [Fact]
+        public void Check_expression_set_exception()
+        {
+            _notification.Clear();
+            _notification.AddAlert<SingleValues>(x => x.BoolNull, exception: new Exception("Alert here"));
+
+            var result = _notification.Messages.First();
+
+            Assert.Equal("Alert here", result.Exception.Message);
+            Assert.Equal(ValidationType.Alert, result.Type);
+        }
+
+        [Fact]
+        public void Check_default()
+        {
+            _notification.Clear();
+            _notification.AddAlert("Message here");
+
+            var result = _notification.Messages.First();
+
+            Assert.Null(result.Reference);
+            Assert.Equal("Message here", result.Message);
+            Assert.Null(result.Exception);
+            Assert.Equal(ValidationType.Alert, result.Type);
+        }
+
+        [Fact]
+        public void Check_setting_all_values()
+        {
+            _notification.Clear();
+            _notification.AddAlert("Message here", reference: "reference", exception: new Exception("Alert here"));
+
+            var result = _notification.Messages.First();
+
+            Assert.Equal("reference", result.Reference);
+            Assert.Equal("Message here", result.Message);
+            Assert.Equal("Alert here", result.Exception.Message);
+            Assert.Equal(ValidationType.Alert, result.Type);
+        }
+    }
+}
---
+++ test/ValidationNotificationTest/AddFatalTest.cs
@@ -0,0 +1,77 @@
+﻿using Xunit;
+using System;
+using System.Linq;
+using BitHelp.Core.Extend;
+using System.Linq.Expressions;
+using BitHelp.Core.Validation.Test.Resources;
+
+namespace BitHelp.Core.Validation.Test.ValidationNotificationTest
+{
+    public class AddFatalTest
+    {
+        readonly ValidationNotification _notification = new ValidationNotification();
+
+        private string GetReference<T>(Expression<Func<T, object>> expression)
+        {
+            return expression.PropertyTrail();
+        }
+        
+        private string GetDisplay<T>(Expression<Func<T, object>> expression)
+        {
+            return expression.PropertyDisplay();
+        }
+
+        [Fact]
+        public void Check_expression_default()
+        {
+            _notification.Clear();
+            _notification.AddFatal<SingleValues>(x => x.BoolNull, new Exception("Error here"));
+
+            var reference = GetReference<SingleValues>(x => x.BoolNull);
+            var result = _notification.Messages.First();
+
+            Assert.Equal(reference, result.Reference);
+            Assert.Equal("Error here", result.Message);
+            Assert.Equal("Error here", result.Exception.Message);
+            Assert.Equal(ValidationType.Fatal, result.Type);
+        }
+
+        [Fact]
+        public void Check_expression_set_reference()
+        {
+            _notification.Clear();
+            _notification.AddFatal<SingleValues>(x => x.BoolNull, new Exception("Error here"), reference: "reference");
+
+            var result = _notification.Messages.First();
+
+            Assert.Equal("reference", result.Reference);
+            Assert.Equal(ValidationType.Fatal, result.Type);
+        }
+        [Fact]
+        public void Check_default()
+        {
+            _notification.Clear();
+            _notification.AddFatal(new Exception("Error here"));
+
+            var result = _notification.Messages.First();
+
+            Assert.Null(result.Reference);
+            Assert.Equal("Error here", result.Message);
+            Assert.Equal("Error here", result.Exception.Message);
+            Assert.Equal(ValidationType.Fatal, result.Type);
+        }
+
+        [Fact]
+        public void Check_setting_all_values()
+        {
+            _notification.Clear();
+            _notification.AddFatal(new Exception("Error here"), reference: "reference");
+
+            var result = _notification.Messages.First();
+
+            Assert.Equal("reference", result.Reference);
+            Assert.Equal("Error here", result.Message);
+            Assert.Equal(ValidationType.Fatal, result.Type);
+        }
+    }
+}
