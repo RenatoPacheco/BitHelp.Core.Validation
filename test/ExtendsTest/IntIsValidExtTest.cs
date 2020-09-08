a149d78fb7f6888856f3877861ee67167b07c0b9
Renato Pacheco <renato.pacheco@adm.unip.br>
2020-09-08 15:41:53 -0300

    Adding integer list test

--- test/ExtendsTest/IntIsValidExtTest.cs
+++ test/ExtendsTest/IntIsValidExtTest.cs
@@ -1,5 +1,6 @@
 ﻿using BitHelp.Core.Validation.Extends;
 using BitHelp.Core.Validation.Test.Resources;
+using System;
 using Xunit;
 
 namespace BitHelp.Core.Validation.Test.ExtendsTest
@@ -56,6 +57,92 @@ namespace BitHelp.Core.Validation.Test.ExtendsTest
 
             _notification.Clear();
             _notification.IntIsValid(single, x => x.IntNull);
+            Assert.True(_notification.IsValid());
+        }
+
+        [Fact]
+        public void Check_ignore_list_empty()
+        {
+            var array = new ArrayValues
+            {
+                String = Array.Empty<string>()
+            };
+
+            _notification.Clear();
+            _notification.IntIsValid(array.String);
+            Assert.True(_notification.IsValid());
+
+            _notification.Clear();
+            _notification.IntIsValid(array, x => x.String);
+            Assert.True(_notification.IsValid());
+        }
+
+        [Fact]
+        public void Check_ignore_list_null()
+        {
+            var array = new ArrayValues
+            {
+                String = null
+            };
+
+            _notification.Clear();
+            _notification.IntIsValid(array.String);
+            Assert.True(_notification.IsValid());
+
+            _notification.Clear();
+            _notification.IntIsValid(array, x => x.String);
+            Assert.True(_notification.IsValid());
+        }
+
+        [Fact]
+        public void Check_list_text_invalid()
+        {
+            var array = new ArrayValues
+            {
+                String = new string[] { "abc" }
+            };
+
+            _notification.Clear();
+            _notification.IntIsValid(array.String);
+            Assert.False(_notification.IsValid());
+
+            _notification.Clear();
+            _notification.IntIsValid(array, x => x.String);
+            Assert.False(_notification.IsValid());
+        }
+
+
+        [Fact]
+        public void Check_list_item_null_invalid()
+        {
+            var array = new ArrayValues
+            {
+                String = new string[] { null }
+            };
+
+            _notification.Clear();
+            _notification.IntIsValid(array.String);
+            Assert.False(_notification.IsValid());
+
+            _notification.Clear();
+            _notification.IntIsValid(array, x => x.String);
+            Assert.False(_notification.IsValid());
+        }
+
+        [Fact]
+        public void Check_list_number_valid()
+        {
+            var array = new ArrayValues
+            {
+                String = new string[] { "123" }
+            };
+
+            _notification.Clear();
+            _notification.IntIsValid(array.String);
+            Assert.True(_notification.IsValid());
+
+            _notification.Clear();
+            _notification.IntIsValid(array, x => x.String);
             Assert.True(_notification.IsValid());
         }
     }
