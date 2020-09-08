39db56b1615805e2d6f86ac28b12b2588d060285
Renato Pacheco <renato.pacheco@adm.unip.br>
2020-09-08 15:45:48 -0300

    Adding non-empty value list test

--- test/ExtendsTest/NotEmptyIsValidExtTest.cs
+++ test/ExtendsTest/NotEmptyIsValidExtTest.cs
@@ -1,5 +1,6 @@
 ﻿using BitHelp.Core.Validation.Extends;
 using BitHelp.Core.Validation.Test.Resources;
+using System;
 using Xunit;
 
 namespace BitHelp.Core.Validation.Test.ExtendsTest
@@ -107,6 +108,92 @@ namespace BitHelp.Core.Validation.Test.ExtendsTest
 
             _notification.Clear();
             _notification.NotEmptyIsValid(single, x => x.String, true);
+            Assert.True(_notification.IsValid());
+        }
+
+        [Fact]
+        public void Check_ignore_list_empty()
+        {
+            var array = new ArrayValues
+            {
+                String = Array.Empty<string>()
+            };
+
+            _notification.Clear();
+            _notification.NotEmptyIsValid(array.String);
+            Assert.True(_notification.IsValid());
+
+            _notification.Clear();
+            _notification.NotEmptyIsValid(array, x => x.String);
+            Assert.True(_notification.IsValid());
+        }
+
+        [Fact]
+        public void Check_ignore_list_null()
+        {
+            var array = new ArrayValues
+            {
+                String = null
+            };
+
+            _notification.Clear();
+            _notification.NotEmptyIsValid(array.String);
+            Assert.True(_notification.IsValid());
+
+            _notification.Clear();
+            _notification.NotEmptyIsValid(array, x => x.String);
+            Assert.True(_notification.IsValid());
+        }
+
+        [Fact]
+        public void Check_list_item_empty_invalid()
+        {
+            var array = new ArrayValues
+            {
+                String = new string[] { "             " }
+            };
+
+            _notification.Clear();
+            _notification.NotEmptyIsValid(array.String);
+            Assert.False(_notification.IsValid());
+
+            _notification.Clear();
+            _notification.NotEmptyIsValid(array, x => x.String);
+            Assert.False(_notification.IsValid());
+        }
+
+
+        [Fact]
+        public void Check_list_item_null_invalid()
+        {
+            var array = new ArrayValues
+            {
+                String = new string[] { null }
+            };
+
+            _notification.Clear();
+            _notification.NotEmptyIsValid(array.String);
+            Assert.False(_notification.IsValid());
+
+            _notification.Clear();
+            _notification.NotEmptyIsValid(array, x => x.String);
+            Assert.False(_notification.IsValid());
+        }
+
+        [Fact]
+        public void Check_list_valid()
+        {
+            var array = new ArrayValues
+            {
+                String = new string[] { "123" }
+            };
+
+            _notification.Clear();
+            _notification.NotEmptyIsValid(array.String);
+            Assert.True(_notification.IsValid());
+
+            _notification.Clear();
+            _notification.NotEmptyIsValid(array, x => x.String);
             Assert.True(_notification.IsValid());
         }
     }
