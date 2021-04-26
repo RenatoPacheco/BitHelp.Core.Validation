6010ef4ef846e146f8ad0adbb1e0d282265b95ba
Renato Pacheco <renato.pacheco@outlook.com>
2021-04-26 19:30:15 -0300

    Add CpfIsValid to ISelfValidation

--- src/Extends/CpfIsValidExt.cs
+++ src/Extends/CpfIsValidExt.cs
@@ -8,6 +8,37 @@ namespace BitHelp.Core.Validation.Extends
 {
     public static class CpfIsValidExt
     {
+        #region To ISelfValidation
+
+        public static ValidationNotification CpfIsValid<T, P>(
+            this T source, Expression<Func<T, P>> expression)
+            where T : ISelfValidation
+        {
+            return source.CpfIsValid(
+                source.GetStructureToValidate(expression));
+        }
+
+        public static ValidationNotification CpfIsValid<T>(
+            this T source, object value)
+            where T : ISelfValidation
+        {
+            return source.CpfIsValid(new StructureToValidate
+            {
+                Value = value,
+                Display = Resource.DisplayValue,
+                Reference = null
+            });
+        }
+
+        public static ValidationNotification CpfIsValid<T>(
+            this T source, IStructureToValidate data)
+            where T : ISelfValidation
+        {
+            return source.Notifications.CpfIsValid(data);
+        }
+
+        #endregion
+
         public static ValidationNotification CpfIsValid<T, P>(
             this ValidationNotification source, T data, Expression<Func<T, P>> expression)
         {
---
+++ test/ExtendsTest/CpfIsValidExtTest.cs
@@ -0,0 +1,75 @@
+﻿using BitHelp.Core.Validation.Extends;
+using BitHelp.Core.Validation.Test.Resources;
+using Xunit;
+
+namespace BitHelp.Core.Validation.Test.ExtendsTest
+{
+    public class CpfIsValidExtTest
+    {
+        readonly ValidationNotification _notification = new ValidationNotification();
+
+        [Fact]
+        public void Check_is_valid()
+        {
+            var single = new SingleValues
+            {
+                String = "222.704.930-87"
+            };
+
+            _notification.Clear();
+            _notification.CpfIsValid(single.String);
+            Assert.True(_notification.IsValid());
+
+            _notification.Clear();
+            _notification.CpfIsValid(single, x => x.String);
+            Assert.True(_notification.IsValid());
+
+            single.Notifications.Clear();
+            single.CpfIsValid(x => x.String);
+            Assert.True(single.IsValid());
+        }
+
+
+        [Fact]
+        public void Check_is_invalid()
+        {
+            var single = new SingleValues
+            {
+                String = "222.704.930-00"
+            };
+
+            _notification.Clear();
+            _notification.CpfIsValid(single.String);
+            Assert.False(_notification.IsValid());
+
+            _notification.Clear();
+            _notification.CpfIsValid(single, x => x.String);
+            Assert.False(_notification.IsValid());
+
+            single.Notifications.Clear();
+            single.CpfIsValid(x => x.String);
+            Assert.False(single.IsValid());
+        }
+
+        [Fact]
+        public void Check_ignore_null()
+        {
+            var single = new SingleValues
+            {
+                String = null
+            };
+
+            _notification.Clear();
+            _notification.CpfIsValid(single.String);
+            Assert.True(_notification.IsValid());
+
+            _notification.Clear();
+            _notification.CpfIsValid(single, x => x.String);
+            Assert.True(_notification.IsValid());
+
+            single.Notifications.Clear();
+            single.CpfIsValid(x => x.String);
+            Assert.True(single.IsValid());
+        }
+    }
+}
