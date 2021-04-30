d3b718fae57635a241ba5fad055ba0dd717c7b0b
Renato Pacheco <renato.pacheco@outlook.com>
2021-04-30 09:32:31 -0300

    Fixing the RemoveAtReference extension

--- src/Extends/RemoveAtReferenceExt.cs
+++ src/Extends/RemoveAtReferenceExt.cs
@@ -1,4 +1,5 @@
 ﻿using BitHelp.Core.Extend;
+using BitHelp.Core.Validation.Helpers;
 using System;
 using System.Linq.Expressions;
 
@@ -7,10 +8,20 @@ namespace BitHelp.Core.Validation.Extends
     public static class RemoveAtReferenceExt
     {
         public static ValidationNotification RemoveAtReference<T, P>(
-            this ValidationNotification source, Expression<Func<T, P>> expression)
+            this T source, Expression<Func<T, P>> expression)
+            where T : ISelfValidation
         {
             string reference = expression.PropertyTrail();
-            source.RemoveAtReference(reference);
+            source.Notifications.RemoveAtReference(reference);
+
+            return source.Notifications;
+        }
+
+        public static ValidationNotification RemoveAtReference<T, P>(
+            this ValidationNotification source, T data, Expression<Func<T, P>> expression)
+        {
+            var validate = data.GetStructureToValidate(expression);
+            source.RemoveAtReference(validate.Reference);
 
             return source;
         }
--- test/ExtendsTest/RemoveAtReferenceExtTest.cs
+++ test/ExtendsTest/RemoveAtReferenceExtTest.cs
@@ -9,7 +9,7 @@ namespace BitHelp.Core.Validation.Test.ExtendsTest
         readonly ValidationNotification _notification = new ValidationNotification();
 
         [Fact]
-        public void Check_date_pt_BR_valid()
+        public void Check_if_the_reference_has_been_removed_by_the_validationNotification()
         {
             var single = new SingleValues
             {
@@ -31,6 +31,56 @@ namespace BitHelp.Core.Validation.Test.ExtendsTest
             _notification.RemoveAtReference<SingleValues>(x => x.IntNull);
             Assert.DoesNotContain(_notification.Messages, x => x.Reference == nameof(single.Int));
             Assert.DoesNotContain(_notification.Messages, x => x.Reference == nameof(single.IntNull));
+        }
+
+        [Fact]
+        public void Check_if_the_reference_has_been_removed_by_the_expression_in_this_object()
+        {
+            var single = new SingleValues
+            {
+                Int = 100,
+                IntNull = 100
+            };
+
+            _notification.Clear();
+            _notification.MaxNumberIsValid(single, x => x.Int, 10);
+            _notification.MaxNumberIsValid(single, x => x.IntNull, 10);
+
+            Assert.Contains(_notification.Messages, x => x.Reference == nameof(single.Int));
+            Assert.Contains(_notification.Messages, x => x.Reference == nameof(single.IntNull));
+
+            _notification.RemoveAtReference(single, x => x.Int);
+            Assert.DoesNotContain(_notification.Messages, x => x.Reference == nameof(single.Int));
+            Assert.Contains(_notification.Messages, x => x.Reference == nameof(single.IntNull));
+
+            _notification.RemoveAtReference(single, x => x.IntNull);
+            Assert.DoesNotContain(_notification.Messages, x => x.Reference == nameof(single.Int));
+            Assert.DoesNotContain(_notification.Messages, x => x.Reference == nameof(single.IntNull));
+        }
+
+        [Fact]
+        public void Check_if_the_reference_has_been_removed_by_the_iSelfValidation()
+        {
+            var single = new SingleValues
+            {
+                Int = 100,
+                IntNull = 100
+            };
+
+            single.Notifications.Clear();
+            single.MaxNumberIsValid(x => x.Int, 10);
+            single.MaxNumberIsValid(x => x.IntNull, 10);
+
+            Assert.Contains(single.Notifications.Messages, x => x.Reference == nameof(single.Int));
+            Assert.Contains(single.Notifications.Messages, x => x.Reference == nameof(single.IntNull));
+
+            single.RemoveAtReference(x => x.Int);
+            Assert.DoesNotContain(single.Notifications.Messages, x => x.Reference == nameof(single.Int));
+            Assert.Contains(single.Notifications.Messages, x => x.Reference == nameof(single.IntNull));
+
+            single.RemoveAtReference(x => x.IntNull);
+            Assert.DoesNotContain(single.Notifications.Messages, x => x.Reference == nameof(single.Int));
+            Assert.DoesNotContain(single.Notifications.Messages, x => x.Reference == nameof(single.IntNull));
         }
     }
 }
