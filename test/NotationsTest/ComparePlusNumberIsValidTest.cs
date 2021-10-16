4a4093c820d4dfed07c3b341d5499ca1f477fbd4
Renato Pacheco <renato.pacheco@outlook.com>
2021-10-16 18:07:21 -0300

    Simplifying tests of the notation ComparePlusNumberIsValid

--- test/NotationsTest/ComparePlusNumberIsValidTest.cs
+++ test/NotationsTest/ComparePlusNumberIsValidTest.cs
@@ -8,88 +8,48 @@ namespace BitHelp.Core.Validation.Test.NotationsTest
     public class ComparePlusNumberIsValidTest
     {
         [ComparePlusNumberIsValid(nameof(Compare))]
-        public int? Value { get; set; }
+        public object Value { get; set; }
 
-        public int? Compare { get; set; }
+        public object Compare { get; set; }
 
-        [Fact]
-        public void Check_all_null_valid()
-        {
-            var model = new ComparePlusNumberIsValidTest();
-            var context = new ValidationContext(model);
-            var results = new List<ValidationResult>();
-            var isValid = Validator.TryValidateObject(model, context, results, true);
-            Assert.True(isValid);
-        }
-
-        [Fact]
-        public void Check_compare_null_valid()
+        [Theory]
+        [InlineData(456, null)]
+        [InlineData(null, 456)]
+        [InlineData(null, null)]
+        [InlineData(456, 123)]
+        [InlineData("456", "123")]
+        [InlineData("abc", 456)]
+        [InlineData(456, "abcd")]
+        [InlineData("abc", "abcd")]
+        public void Check_value_is_valid(object value, object compare)
         {
             var model = new ComparePlusNumberIsValidTest()
             {
-                Value = 123,
-                Compare = null
+                Value = value,
+                Compare = compare
             };
             var context = new ValidationContext(model);
             var results = new List<ValidationResult>();
-            var isValid = Validator.TryValidateObject(model, context, results, true);
-            Assert.True(isValid);
+            var result = Validator.TryValidateObject(model, context, results, true);
+            Assert.True(result);
         }
 
-        [Fact]
-        public void Check_value_null_valid()
+        [Theory]
+        [InlineData(456, 456)]
+        [InlineData(123, 456)]
+        [InlineData("456", "456")]
+        [InlineData("123", "456")]
+        public void Check_value_not_is_valid(object value, object compare)
         {
             var model = new ComparePlusNumberIsValidTest()
             {
-                Value = null,
-                Compare = 123
+                Value = value,
+                Compare = compare
             };
             var context = new ValidationContext(model);
             var results = new List<ValidationResult>();
-            var isValid = Validator.TryValidateObject(model, context, results, true);
-            Assert.True(isValid);
-        }
-
-        [Fact]
-        public void Check_value_less_invalid()
-        {
-            var model = new ComparePlusNumberIsValidTest()
-            {
-                Value = 123,
-                Compare = 456
-            };
-            var context = new ValidationContext(model);
-            var results = new List<ValidationResult>();
-            var isValid = Validator.TryValidateObject(model, context, results, true);
-            Assert.False(isValid);
-        }
-
-        [Fact]
-        public void Check_value_equal_invalid()
-        {
-            var model = new ComparePlusNumberIsValidTest()
-            {
-                Value = 123,
-                Compare = 123
-            };
-            var context = new ValidationContext(model);
-            var results = new List<ValidationResult>();
-            var isValid = Validator.TryValidateObject(model, context, results, true);
-            Assert.False(isValid);
-        }
-
-        [Fact]
-        public void Check_value_plus_valid()
-        {
-            var model = new ComparePlusNumberIsValidTest()
-            {
-                Value = 456,
-                Compare = 123
-            };
-            var context = new ValidationContext(model);
-            var results = new List<ValidationResult>();
-            var isValid = Validator.TryValidateObject(model, context, results, true);
-            Assert.True(isValid);
+            var result = Validator.TryValidateObject(model, context, results, true);
+            Assert.False(result);
         }
     }
 }
