4d36d76e09e368d90be3c6fdf0c2b2bfad9ace18
Renato Pacheco <renato.pacheco@outlook.com>
2022-07-27 16:40:19 -0300

    Adding testing exception

---
+++ test/Resources/CustonException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitHelp.Core.Validation.Test.Resources
+{
+    public class CustonException : Exception
+    {
+        public CustonException(string message, string stackTrace = null)
+        {
+            _message = message;
+            _stackTrace = stackTrace;
+        }
+
+
+        private string _message;
+        public override string Message => _message;
+
+        private string _stackTrace;
+        public override string StackTrace => _stackTrace;
+    }
+}
---
+++ test/ValidationMessageTest/ConstructorTest.cs
@@ -0,0 +1,31 @@
+﻿using BitHelp.Core.Validation.Test.Resources;
+using Xunit;
+
+namespace BitHelp.Core.Validation.Test.ValidationMessageTest
+{
+    public class ConstructorTest
+    {
+        [Theory]
+        [InlineData(null, null, null, ValidationType.Error, "Exception")]
+        [InlineData(null, "Stack Trace", null, ValidationType.Error, "Stack Trace")]
+        [InlineData("Message", "Stack Trace", null, ValidationType.Error, "Message")]
+        public void Check_exception_values(
+            string message, string stackTrace, 
+            string reference, ValidationType type, string expected)
+        {
+            try
+            {
+                throw new CustonException(message, stackTrace);
+            }
+            catch (CustonException ex)
+            {
+                ValidationMessage result = new(ex, reference, type);
+
+                Assert.Equal(result.Message, expected);
+                Assert.Equal(result.Type, type);
+                Assert.Equal(result.Reference, reference);
+
+            }
+        }
+    }
+}
