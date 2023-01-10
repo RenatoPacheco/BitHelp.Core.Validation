bffac335a45c178d3af3737ef51ac429296ea3d5
Renato Pacheco <renato.pacheco@adm.unip.br>
2023-01-10 19:36:29 -0300

    Adding UshortIsValid

---
+++ src/Extends/UshortIsValidExt.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq.Expressions;
+using BitHelp.Core.Validation.Helpers;
+using BitHelp.Core.Validation.Notations;
+using BitHelp.Core.Validation.Resources;
+
+namespace BitHelp.Core.Validation.Extends
+{
+    public static class UshortIsValidExt
+    {
+        #region To ISelfValidation
+
+        public static ValidationNotification UshortIsValid<T, P>(
+            this T source, Expression<Func<T, P>> expression)
+            where T : ISelfValidation
+        {
+            return source.UshortIsValid(
+                source.GetStructureToValidate(expression));
+        }
+
+        public static ValidationNotification UshortIsValid<T>(
+            this T source, object value)
+            where T : ISelfValidation
+        {
+            return source.UshortIsValid(new StructureToValidate
+            {
+                Value = value,
+                Display = Resource.DisplayValue,
+                Reference = null
+            });
+        }
+
+        public static ValidationNotification UshortIsValid<T>(
+            this T source, IStructureToValidate data)
+            where T : ISelfValidation
+        {
+            return source.Notifications.UshortIsValid(data);
+        }
+
+        #endregion
+
+        #region ValidationNotification
+
+        public static ValidationNotification UshortIsValid<T, P>(
+            this ValidationNotification source, T data, Expression<Func<T, P>> expression)
+        {
+            return source.UshortIsValid(
+                data.GetStructureToValidate(expression));
+        }
+
+        public static ValidationNotification UshortIsValid(
+            this ValidationNotification source, object value)
+        {
+            return source.UshortIsValid(new StructureToValidate
+            {
+                Value = value,
+                Display = Resource.DisplayValue,
+                Reference = null
+            });
+        }
+
+        public static ValidationNotification UshortIsValid(
+            this ValidationNotification source, IStructureToValidate data)
+        {
+            source.CleanLastMessage();
+            UshortIsValidAttribute validation = new UshortIsValidAttribute();
+            if (!validation.IsValid(data.Value))
+            {
+                string text = validation.FormatErrorMessage(data.Display);
+                var message = new ValidationMessage(text, data.Reference);
+                source.SetLastMessage(message, data.Display);
+                source.Add(message);
+            }
+            return source;
+        }
+
+        #endregion
+    }
+}
---
+++ src/Notations/UshortIsValidAttribute.cs
@@ -0,0 +1,21 @@
+﻿using BitHelp.Core.Validation.Resources;
+using System;
+
+namespace BitHelp.Core.Validation.Notations
+{
+    [AttributeUsage(AttributeTargets.Property |
+           AttributeTargets.Field, AllowMultiple = false)]
+    public class UshortIsValidAttribute : ListIsValidAttribute
+    {
+        public UshortIsValidAttribute() : base()
+        {
+            ErrorMessageResourceName = nameof(Resource.XShortInvalid);
+        }
+
+        protected override bool Check(object value)
+        {
+            string input = Convert.ToString(value);
+            return ushort.TryParse(input, out _);
+        }
+    }
+}
