e072ec47f43251cfdccaac027ebc851e274b29e1
Renato Pacheco <renato.pacheco@adm.unip.br>
2023-01-10 20:00:29 -0300

    Adding ByteIsValid

---
+++ src/Notations/ByteIsValidAttribute.cs
@@ -0,0 +1,21 @@
+﻿using BitHelp.Core.Validation.Resources;
+using System;
+
+namespace BitHelp.Core.Validation.Notations
+{
+    [AttributeUsage(AttributeTargets.Property |
+           AttributeTargets.Field, AllowMultiple = false)]
+    public class ByteIsValidAttribute : ListIsValidAttribute
+    {
+        public ByteIsValidAttribute() : base()
+        {
+            ErrorMessageResourceName = nameof(Resource.XByteInvalid);
+        }
+
+        protected override bool Check(object value)
+        {
+            string input = Convert.ToString(value);
+            return byte.TryParse(input, out _);
+        }
+    }
+}
