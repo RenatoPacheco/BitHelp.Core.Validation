71d8c554f1d35d78fb47932ba8f464d2498c7526
Renato Pacheco <renato.pacheco@adm.unip.br>
2023-01-10 19:34:32 -0300

    Adding ShortIsValid

---
+++ src/Extends/ShortIsValidExt.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq.Expressions;
+using BitHelp.Core.Validation.Helpers;
+using BitHelp.Core.Validation.Notations;
+using BitHelp.Core.Validation.Resources;
+
+namespace BitHelp.Core.Validation.Extends
+{
+    public static class ShortIsValidExt
+    {
+        #region To ISelfValidation
+
+        public static ValidationNotification ShortIsValid<T, P>(
+            this T source, Expression<Func<T, P>> expression)
+            where T : ISelfValidation
+        {
+            return source.ShortIsValid(
+                source.GetStructureToValidate(expression));
+        }
+
+        public static ValidationNotification ShortIsValid<T>(
+            this T source, object value)
+            where T : ISelfValidation
+        {
+            return source.ShortIsValid(new StructureToValidate
+            {
+                Value = value,
+                Display = Resource.DisplayValue,
+                Reference = null
+            });
+        }
+
+        public static ValidationNotification ShortIsValid<T>(
+            this T source, IStructureToValidate data)
+            where T : ISelfValidation
+        {
+            return source.Notifications.ShortIsValid(data);
+        }
+
+        #endregion
+
+        #region ValidationNotification
+
+        public static ValidationNotification ShortIsValid<T, P>(
+            this ValidationNotification source, T data, Expression<Func<T, P>> expression)
+        {
+            return source.ShortIsValid(
+                data.GetStructureToValidate(expression));
+        }
+
+        public static ValidationNotification ShortIsValid(
+            this ValidationNotification source, object value)
+        {
+            return source.ShortIsValid(new StructureToValidate
+            {
+                Value = value,
+                Display = Resource.DisplayValue,
+                Reference = null
+            });
+        }
+
+        public static ValidationNotification ShortIsValid(
+            this ValidationNotification source, IStructureToValidate data)
+        {
+            source.CleanLastMessage();
+            ShortIsValidAttribute validation = new ShortIsValidAttribute();
+            if (!validation.IsValid(data.Value))
+            {
+                string text = validation.FormatErrorMessage(data.Display);
+                var message = new ValidationMessage(text, data.Reference);
+                source.SetLastMessage(message, data.Display);
+                source.Add(message);
+            }
+            return source;
+        }
+
+        #endregion
+    }
+}
---
+++ src/Notations/ShortIsValidAttribute.cs
@@ -0,0 +1,21 @@
+﻿using BitHelp.Core.Validation.Resources;
+using System;
+
+namespace BitHelp.Core.Validation.Notations
+{
+    [AttributeUsage(AttributeTargets.Property |
+           AttributeTargets.Field, AllowMultiple = false)]
+    public class ShortIsValidAttribute : ListIsValidAttribute
+    {
+        public ShortIsValidAttribute() : base()
+        {
+            ErrorMessageResourceName = nameof(Resource.XShortInvalid);
+        }
+
+        protected override bool Check(object value)
+        {
+            string input = Convert.ToString(value);
+            return short.TryParse(input, out _);
+        }
+    }
+}
