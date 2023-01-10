03cf881209eb50bb1a0f2a56b361e642ad99de25
Renato Pacheco <renato.pacheco@adm.unip.br>
2023-01-10 19:40:28 -0300

    Adding UlongIsValid

---
+++ src/Extends/UlongIsValidExt.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq.Expressions;
+using BitHelp.Core.Validation.Helpers;
+using BitHelp.Core.Validation.Notations;
+using BitHelp.Core.Validation.Resources;
+
+namespace BitHelp.Core.Validation.Extends
+{
+    public static class UlongIsValidExt
+    {
+        #region To ISelfValidation
+
+        public static ValidationNotification UlongIsValid<T, P>(
+            this T source, Expression<Func<T, P>> expression)
+            where T : ISelfValidation
+        {
+            return source.UlongIsValid(
+                source.GetStructureToValidate(expression));
+        }
+
+        public static ValidationNotification UlongIsValid<T>(
+            this T source, object value)
+            where T : ISelfValidation
+        {
+            return source.UlongIsValid(new StructureToValidate
+            {
+                Value = value,
+                Display = Resource.DisplayValue,
+                Reference = null
+            });
+        }
+
+        public static ValidationNotification UlongIsValid<T>(
+            this T source, IStructureToValidate data)
+            where T : ISelfValidation
+        {
+            return source.Notifications.UlongIsValid(data);
+        }
+
+        #endregion
+
+        public static ValidationNotification UlongIsValid<T, P>(
+            this ValidationNotification source, T data, Expression<Func<T, P>> expression)
+        {
+            return source.UlongIsValid(
+                data.GetStructureToValidate(expression));
+        }
+
+        public static ValidationNotification UlongIsValid(
+            this ValidationNotification source, object value)
+        {
+            return source.UlongIsValid(new StructureToValidate
+            {
+                Value = value,
+                Display = Resource.DisplayValue,
+                Reference = null
+            });
+        }
+
+        public static ValidationNotification UlongIsValid(
+            this ValidationNotification source, IStructureToValidate data)
+        {
+            source.CleanLastMessage();
+            UlongIsValidAttribute validation = new UlongIsValidAttribute();
+            if (!validation.IsValid(data.Value))
+            {
+                string text = validation.FormatErrorMessage(data.Display);
+                var message = new ValidationMessage(text, data.Reference);
+                source.SetLastMessage(message, data.Display);
+                source.Add(message);
+            }
+            return source;
+        }
+    }
+}
---
+++ src/Notations/UlongIsValidAttribute.cs
@@ -0,0 +1,21 @@
+﻿using BitHelp.Core.Validation.Resources;
+using System;
+
+namespace BitHelp.Core.Validation.Notations
+{
+    [AttributeUsage(AttributeTargets.Property |
+           AttributeTargets.Field, AllowMultiple = false)]
+    public class UlongIsValidAttribute : ListIsValidAttribute
+    {
+        public UlongIsValidAttribute() : base()
+        {
+            ErrorMessageResourceName = nameof(Resource.XLongInvalid);
+        }
+
+        protected override bool Check(object value)
+        {
+            string input = Convert.ToString(value);
+            return ulong.TryParse(input, out _);
+        }
+    }
+}
