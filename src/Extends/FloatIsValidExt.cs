8926607ef8880bbc1aae0e27cd786a6d060a3bf3
Renato Pacheco <renato.pacheco@adm.unip.br>
2023-01-10 19:32:34 -0300

    Adding FloatIsValid

---
+++ src/Extends/FloatIsValidExt.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq.Expressions;
+using BitHelp.Core.Validation.Helpers;
+using BitHelp.Core.Validation.Notations;
+using BitHelp.Core.Validation.Resources;
+
+namespace BitHelp.Core.Validation.Extends
+{
+    public static class FloatIsValidExt
+    {
+        #region To ISelfValidation
+
+        public static ValidationNotification FloatIsValid<T, P>(
+            this T source, Expression<Func<T, P>> expression)
+            where T : ISelfValidation
+        {
+            return source.FloatIsValid(
+                source.GetStructureToValidate(expression));
+        }
+
+        public static ValidationNotification FloatIsValid<T>(
+            this T source, object value)
+            where T : ISelfValidation
+        {
+            return source.FloatIsValid(new StructureToValidate
+            {
+                Value = value,
+                Display = Resource.DisplayValue,
+                Reference = null
+            });
+        }
+
+        public static ValidationNotification FloatIsValid<T>(
+            this T source, IStructureToValidate data)
+            where T : ISelfValidation
+        {
+            return source.Notifications.FloatIsValid(data);
+        }
+
+        #endregion
+
+        #region ValidationNotification
+
+        public static ValidationNotification FloatIsValid<T, P>(
+            this ValidationNotification source, T data, Expression<Func<T, P>> expression)
+        {
+            return source.FloatIsValid(
+                data.GetStructureToValidate(expression));
+        }
+
+        public static ValidationNotification FloatIsValid(
+            this ValidationNotification source, object value)
+        {
+            return source.FloatIsValid(new StructureToValidate
+            {
+                Value = value,
+                Display = Resource.DisplayValue,
+                Reference = null
+            });
+        }
+
+        public static ValidationNotification FloatIsValid(
+            this ValidationNotification source, IStructureToValidate data)
+        {
+            source.CleanLastMessage();
+            FloatIsValidAttribute validation = new FloatIsValidAttribute();
+            if (!validation.IsValid(data.Value))
+            {
+                string text = validation.FormatErrorMessage(data.Display);
+                var message = new ValidationMessage(text, data.Reference);
+                source.SetLastMessage(message, data.Display);
+                source.Add(message);
+            }
+            return source;
+        }
+
+        #endregion
+    }
+}
---
+++ src/Notations/FloatIsValidAttribute.cs
@@ -0,0 +1,21 @@
+﻿using BitHelp.Core.Validation.Resources;
+using System;
+
+namespace BitHelp.Core.Validation.Notations
+{
+    [AttributeUsage(AttributeTargets.Property |
+           AttributeTargets.Field, AllowMultiple = false)]
+    public class FloatIsValidAttribute : ListIsValidAttribute
+    {
+        public FloatIsValidAttribute() : base()
+        {
+            ErrorMessageResourceName = nameof(Resource.XDecimalInvalid);
+        }
+
+        protected override bool Check(object value)
+        {
+            string input = Convert.ToString(value);
+            return float.TryParse(input, out _);
+        }
+    }
+}
