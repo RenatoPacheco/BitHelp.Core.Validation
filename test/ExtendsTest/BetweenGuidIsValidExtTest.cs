67d8462800f21f84b9786915ac0724e8b4055062
Renato Pacheco <renato.pacheco@adm.unip.br>
2023-01-10 07:02:07 -0300

    Testing BetweenGuidIsValid

---
+++ test/ExtendsTest/BetweenGuidIsValidExtTest.cs
@@ -0,0 +1,168 @@
+﻿using BitHelp.Core.Validation.Extends;
+using BitHelp.Core.Validation.Test.Resources;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace BitHelp.Core.Validation.Test.ExtendsTest
+{
+    public class BetweenGuidIsValidExtTest
+    {
+        public BetweenGuidIsValidExtTest()
+        {
+            _options = new Guid[]
+            {
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                Guid.NewGuid()
+            };
+        }
+
+        private readonly ValidationNotification _notification = new();
+        private readonly Guid[] _options;
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void Check_contain_first_value_valid(bool denay)
+        {
+            SingleValues single = new()
+            {
+                String = _options.First().ToString()
+            };
+
+            _notification.Clear();
+            _notification.BetweenGuidIsValid(single.String, _options, denay);
+            Assert.Equal(!denay, _notification.IsValid());
+
+            _notification.Clear();
+            _notification.BetweenGuidIsValid(single, x => x.String, _options, denay);
+            Assert.Equal(!denay, _notification.IsValid());
+
+            single.Notifications.Clear();
+            single.BetweenGuidIsValid(x => x.String, _options, denay);
+            Assert.Equal(!denay, single.IsValid());
+
+            single.Notifications.Clear();
+            single.BetweenGuidIsValid(single.String, _options, denay);
+            Assert.Equal(!denay, single.IsValid());
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void Check_not_contain_value_invalid(bool denay)
+        {
+            SingleValues single = new()
+            {
+                String = Guid.NewGuid().ToString()
+            };
+
+            _notification.Clear();
+            _notification.BetweenGuidIsValid(single.String, _options, denay);
+            Assert.Equal(denay, _notification.IsValid());
+
+            _notification.Clear();
+            _notification.BetweenGuidIsValid(single, x => x.String, _options, denay);
+            Assert.Equal(denay, _notification.IsValid());
+
+            single.Notifications.Clear();
+            single.BetweenGuidIsValid(x => x.String, _options, denay);
+            Assert.Equal(denay, single.IsValid());
+
+            single.Notifications.Clear();
+            single.BetweenGuidIsValid(single.String, _options, denay);
+            Assert.Equal(denay, single.IsValid());
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void Check_text_invalid(bool denay)
+        {
+            SingleValues single = new()
+            {
+                String = "text"
+            };
+
+            _notification.Clear();
+            _notification.BetweenGuidIsValid(single.String, _options, denay);
+            Assert.False(_notification.IsValid());
+
+            _notification.Clear();
+            _notification.BetweenGuidIsValid(single, x => x.String, _options, denay);
+            Assert.False(_notification.IsValid());
+
+            single.Notifications.Clear();
+            single.BetweenGuidIsValid(x => x.String, _options, denay);
+            Assert.False(single.IsValid());
+
+            single.Notifications.Clear();
+            single.BetweenGuidIsValid(single.String, _options, denay);
+            Assert.False(single.IsValid());
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void Check_ignore_null_valid(bool denay)
+        {
+            SingleValues single = new()
+            {
+                String = null
+            };
+
+            _notification.Clear();
+            _notification.BetweenGuidIsValid(single.String, _options, denay);
+            Assert.True(_notification.IsValid());
+
+            _notification.Clear();
+            _notification.BetweenGuidIsValid(single, x => x.String, _options, denay);
+            Assert.True(_notification.IsValid());
+
+            single.Notifications.Clear();
+            single.BetweenGuidIsValid(x => x.String, _options, denay);
+            Assert.True(single.IsValid());
+
+            single.Notifications.Clear();
+            single.BetweenGuidIsValid(single.String, _options, denay);
+            Assert.True(single.IsValid());
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void Check_option_null_exception(bool denay)
+        {
+            SingleValues single = new()
+            {
+                String = null
+            };
+
+            Guid[] options = null;
+
+            Assert.Throws<ArgumentException>(() => _notification.BetweenGuidIsValid(single.String, options, denay));
+            Assert.Throws<ArgumentException>(() => _notification.BetweenGuidIsValid(single, x => x.String, options, denay));
+            Assert.Throws<ArgumentException>(() => single.BetweenGuidIsValid(x => x.String, options, denay));
+            Assert.Throws<ArgumentException>(() => single.BetweenGuidIsValid(single.String, options, denay));
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void Check_option_empty_exception(bool denay)
+        {
+            SingleValues single = new()
+            {
+                String = null
+            };
+
+            Guid[] options = Array.Empty<Guid>();
+
+            Assert.Throws<ArgumentException>(() => _notification.BetweenGuidIsValid(single.String, options, denay));
+            Assert.Throws<ArgumentException>(() => _notification.BetweenGuidIsValid(single, x => x.String, options, denay));
+            Assert.Throws<ArgumentException>(() => single.BetweenGuidIsValid(x => x.String, options, denay));
+            Assert.Throws<ArgumentException>(() => single.BetweenGuidIsValid(single.String, options, denay));
+        }
+    }
+}
