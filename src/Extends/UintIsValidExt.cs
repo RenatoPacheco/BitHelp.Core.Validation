757811f447acbba42b36f21bc71066892bf4dfc5
Renato Pacheco <renato.pacheco@adm.unip.br>
2023-01-10 19:38:47 -0300

    Adding UintIsValid

---
+++ src/Extends/UintIsValidExt.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq.Expressions;
+using BitHelp.Core.Validation.Helpers;
+using BitHelp.Core.Validation.Notations;
+using BitHelp.Core.Validation.Resources;
+
+namespace BitHelp.Core.Validation.Extends
+{
+    public static class UintIsValidExt
+    {
+        #region To ISelfValidation
+
+        public static ValidationNotification UintIsValid<T, P>(
+            this T source, Expression<Func<T, P>> expression)
+            where T : ISelfValidation
+        {
+            return source.UintIsValid(
+                source.GetStructureToValidate(expression));
+        }
+
+        public static ValidationNotification UintIsValid<T>(
+            this T source, object value)
+            where T : ISelfValidation
+        {
+            return source.UintIsValid(new StructureToValidate
+            {
+                Value = value,
+                Display = Resource.DisplayValue,
+                Reference = null
+            });
+        }
+
+        public static ValidationNotification UintIsValid<T>(
+            this T source, IStructureToValidate data)
+            where T : ISelfValidation
+        {
+            return source.Notifications.UintIsValid(data);
+        }
+
+        #endregion
+
+        #region ValidationNotification
+
+        public static ValidationNotification UintIsValid<T, P>(
+            this ValidationNotification source, T data, Expression<Func<T, P>> expression)
+        {
+            return source.UintIsValid(
+                data.GetStructureToValidate(expression));
+        }
+
+        public static ValidationNotification UintIsValid(
+            this ValidationNotification source, object value)
+        {
+            return source.UintIsValid(new StructureToValidate
+            {
+                Value = value,
+                Display = Resource.DisplayValue,
+                Reference = null
+            });
+        }
+
+        public static ValidationNotification UintIsValid(
+            this ValidationNotification source, IStructureToValidate data)
+        {
+            source.CleanLastMessage();
+            UintIsValidAttribute validation = new UintIsValidAttribute();
+            if (!validation.IsValid(data.Value))
+            {
+                string text = validation.FormatErrorMessage(data.Display);
+                var message = new ValidationMessage(text, data.Reference);
+                source.SetLastMessage(message, data.Display);
+                source.Add(message);
+            }
+            return source;
+        }
+
+        #endregion
+    }
+}
---
+++ src/Notations/UintIsValidAttribute.cs
@@ -0,0 +1,21 @@
+﻿using BitHelp.Core.Validation.Resources;
+using System;
+
+namespace BitHelp.Core.Validation.Notations
+{
+    [AttributeUsage(AttributeTargets.Property |
+           AttributeTargets.Field, AllowMultiple = false)]
+    public class UintIsValidAttribute : ListIsValidAttribute
+    {
+        public UintIsValidAttribute() : base()
+        {
+            ErrorMessageResourceName = nameof(Resource.XIntInvalid);
+        }
+
+        protected override bool Check(object value)
+        {
+            string input = Convert.ToString(value);
+            return uint.TryParse(input, out _);
+        }
+    }
+}
